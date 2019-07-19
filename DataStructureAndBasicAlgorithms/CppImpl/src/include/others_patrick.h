f439d005368d7437b6a36c98ff711f520e3fd7c9
Patrick <pengcheng.zhang@moji.com>
2019-07-19 18:03:03 +0800

    improved subarray sum

--- DataStructureAndBasicAlgorithms/CppImpl/src/include/others_patrick.h
+++ DataStructureAndBasicAlgorithms/CppImpl/src/include/others_patrick.h
@@ -5,4 +5,12 @@
 #ifndef CPPIMPL_OTHERS_PATRICK_H
 #define CPPIMPL_OTHERS_PATRICK_H
 
+#include <vector>
+namespace patrick{
+    template <class T>
+    T maxSubArraySumImproved(const std::vector<T>& array);
+    template <class T>
+    T maxSubArraySumLinear(const std::vector<T>& array);
+
+}
 #endif //CPPIMPL_OTHERS_PATRICK_H
--- DataStructureAndBasicAlgorithms/CppImpl/src/others_patrick.cpp
+++ DataStructureAndBasicAlgorithms/CppImpl/src/others_patrick.cpp
@@ -2,3 +2,30 @@
 // Created by pengcheng.zhang on 2019-07-10.
 //
 #include "others_patrick.h"
+#include <vector>
+
+template<class T>
+T patrick::maxSubArraySumImproved(const std::vector <T> &array) {
+    T sum = array[0];
+    std::vector <T> subStartSum = new std::vector <T>{array.size()};
+    subStartSum[0] = array[0];
+    for (int i = 1; i < array.size(); i++) {
+        subStartSum[i] = subStartSum[i - 1] + array[i];
+    }
+    for (int i = 1; i < array.size(); i++) {
+        for (int j = i; j < array.size(); j++) {
+            if (subStartSum[j] - subStartSum[i - 1] > sum) {
+                sum=subStartSum[j]-subStartSum[i-1];
+            }
+        }
+    }
+    return sum;
+}
+
+template<class T>
+T patrick:: maxSubArraySumLinear(const std::vector <T> &array) {
+    T sum;
+
+    return sum;
+}
+
