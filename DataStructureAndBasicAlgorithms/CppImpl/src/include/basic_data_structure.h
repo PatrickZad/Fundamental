e19c7937f32da63e0f190523a24a2ad9b69bc1c8
Patrick <pengcheng.zhang@moji.com>
2019-07-30 18:38:21 +0800

    structure framework

--- DataStructureAndBasicAlgorithms/CppImpl/src/include/basic_data_structure.h
+++ DataStructureAndBasicAlgorithms/CppImpl/src/include/basic_data_structure.h
@@ -5,4 +5,72 @@
 #ifndef CPPIMPL_BASIC_DATA_STRUCTURE_H
 #define CPPIMPL_BASIC_DATA_STRUCTURE_H
 
+#include <stdint.h>
+
+namespace patrick{
+
+    template <class T>
+            struct Node{
+            T data;
+            Node<T>& next;
+            };
+    template <class T>
+    class ArrayStack{
+    public:
+        ArrayStack();
+        bool push(T);
+        T pop();
+        int size();
+    private:
+        const static u_int8_t N = 255;
+        T stack[N];
+        u_int8_t length=0;
+    };
+    template <class T>
+    class ArrayQueue{
+        /*
+         * Circular Queue
+         * */
+    public:
+        ArrayQueue();
+        bool enqueue(T);
+        T dequeue();
+        int size();
+    private:
+        const static u_int8_t N = 255;
+        T queue[N];
+        u_int8_t length=0;
+        u_int8_t front=0;
+        u_int8_t next=0;
+    };
+    template <class T>
+    class ArrayList{
+    public:
+        ArrayList();
+        T get(int);
+        bool set(int, T);
+        bool insert(int, T);
+        bool remove(int);
+        int size();
+
+    private:
+        const static u_int8_t N = 255;
+        T list[N];
+        u_int8_t length=0;
+    };
+    template <class T>
+    class LinkedList{
+    public:
+        LinkedList();
+        T get(int);
+        bool set(int, T);
+        bool insert(int, T);
+        bool remove(int);
+        int size();
+
+    private:
+        int length=0;
+        Node<T>& head;
+    };
+}
 #endif //CPPIMPL_BASIC_DATA_STRUCTURE_H
