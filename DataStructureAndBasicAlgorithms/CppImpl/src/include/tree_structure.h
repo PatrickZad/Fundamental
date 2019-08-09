d52c9fa441571a5a29540d51f6b327e04a64e050
Patrick <pengcheng.zhang@moji.com>
2019-08-09 19:30:15 +0800

    binary tree

--- DataStructureAndBasicAlgorithms/CppImpl/src/include/tree_structure.h
+++ DataStructureAndBasicAlgorithms/CppImpl/src/include/tree_structure.h
@@ -4,5 +4,32 @@
 
 #ifndef CPPIMPL_TREE_STRUCTURE_H
 #define CPPIMPL_TREE_STRUCTURE_H
+namespace patrick{
+    template <class T>
+    struct BinaryNode{
+        T* data;
+        BinaryNode* left;
+        BinaryNode* right;
+    };
+    template <class T>
+    class BinaryTree{
+    public:
+        BinaryTree();
+        unsigned int size();
+        virtual T* get(unsigned int index)=0;
+        virtual bool insert(T& data)=0;
+        BinaryNode<T>* head;
+        template < class F>
+        void preOrderTraversal(F& visitFunc);
+        template < class F>
+        void inOrderTraversal(F& visitFunc);
+        template < class F>
+        void postOrderTraversal(F& visitFunc);
+        template < class F>
+        void layerTraversal(F& visitFunc);
+    private:
+        unsigned int length;
+    };
 
+}
 #endif //CPPIMPL_TREE_STRUCTURE_H
--- DataStructureAndBasicAlgorithms/CppImpl/src/tree_structure.cpp
+++ DataStructureAndBasicAlgorithms/CppImpl/src/tree_structure.cpp
@@ -1,4 +1,13 @@
 //
 // Created by pengcheng.zhang on 2019-08-07.
 //
-
+#include "tree_structure.h"
+using namespace patrick;
+template <class T>
+BinaryTree<T>::BinaryTree() {
+    this->length=0;
+}
+template <class T>
+unsigned int BinaryTree<T>::size() {
+    return this->length;
+}
