442bf0672ecf00d8c85bb54ac8be3709aef158aa
Patrick <pengcheng.zhang@moji.com>
2019-09-04 18:51:45 +0800

    auto

---
+++ DataStructureAndBasicAlgorithms/CppImpl/src/include/hash_map.cpp
@@ -0,0 +1,5 @@
+//
+// Created by pengcheng.zhang on 2019/9/4.
+//
+
+#include "hash_map.h"
---
+++ DataStructureAndBasicAlgorithms/CppImpl/src/include/hash_map.h
@@ -0,0 +1,15 @@
+//
+// Created by pengcheng.zhang on 2019/9/4.
+//
+
+#ifndef CPPIMPL_HASH_MAP_H
+#define CPPIMPL_HASH_MAP_H
+namespace patrick{
+
+template <class K. class V>
+class HashMap{
+
+};
+
+}
+#endif //CPPIMPL_HASH_MAP_H
