d687bebd2034479b8da008368ed05fc646e5d172
Patrick <dawnerz@163.com>
2019-10-07 22:13:11 +0800

    array heap and priority queue

--- DataStructureAndBasicAlgorithms/CppImpl/src/include/priority_structure.h
+++ DataStructureAndBasicAlgorithms/CppImpl/src/include/priority_structure.h
@@ -12,67 +12,77 @@ namespace patrick{
         K key;
         V value;
     };
+
     template <class K, class V>
-    class PriorityQueue{
+    class Heap{
     public:
-        PriorityQueue();
+        Heap():length{0}{};
         unsigned int size();
-        bool enqueue(K& key, V& value);
-        Item<K,V> dequeue();
-    private:
-        unsigned int length;
+        virtual void push(K& key, V& value)=0;
+        virtual V pop()=0;
+        virtual V& top()=0;
+    protected:
+        unsigned int length=0;
     };
+
+    template <class K, class V>
+    class ArrayHeap: public Heap<K,V>{
+    private:
+        ExtendableArray<Item<K,V>> array;
+    public:
+        void push(K& key, V& value);
+        V pop();
+        V& top();
+    private:
+        void maxHeapify(unsigned int index);
+        Item<K,V>* parentOf(unsigned int index);
+        Item<K,V>* leftOf(unsigned int index);
+        Item<K,V>* rightOf(unsigned int index);
+    };
+/*
     template <class K, class V>
     struct HeapNode{
         K key;
         V value;
+        HeapNode* parent;
         HeapNode* left;
         HeapNode* right;
     };
 
     template <class K, class V>
-    class Heap{
-    public:
-        Heap();
-        unsigned int size();
-        virtual unsigned int height(K& key)=0;
-        virtual unsigned int depth(K& key)=0;
-        virtual void insert(K& key, V& value)=0;
-        virtual void remove(K& key)=0;
+    class TreeHeap: public Heap<K,V>{
     private:
-        unsigned int length;
-    };
-
-    template <class K, class V>
-    class ArrayHeap:Heap<K,V>{
+        HeapNode<K,V>* root;
     public:
-        void insert(K& key, V& value);
-        Item<K,V> remove(K& key);
-        unsigned int height(K& key);
-        unsigned int depth(K& key);
-    private:
-        ExtendableArray<Item<K,V>> array;
-        unsigned int parentIndexOf(unsigned int index);
-        unsigned int leftIndexOf(unsigned int index);
-        unsigned int rightIndexOf(unsigned int index);
-    };
-
-    template <class K, class V>
-    class TreeHeap:Heap<K,V>{
-    public:
-        TreeHeap();
+        TreeHeap():root{nullptr},end{nullptr}{};
         ~TreeHeap();
         TreeHeap(const TreeHeap& heap);
         TreeHeap(TreeHeap&& heap);
         TreeHeap& operator=(const TreeHeap& heap);
         TreeHeap& operator=(TreeHeap&& heap);
-        void insert(K& key, V& value);
-        Item<K,V> remove(K& key);
-        unsigned int height(K& key);
-        unsigned int depth(K& key);
 
+        void push(K& key, V& value);
+        V pop();
+        V& top();
+
+    };
+    template <class K, class V>
+    void releaseTree(HeapNode<K,V>* root);
+    template <class K, class V>
+    void copyTree(HeapNode<K,V>* destRoot,HeapNode<K,V>* sourRoot);
+    */
+    template <class K, class V>
+    class PriorityQueue{
     private:
-        HeapNode<K,V>* root;
+        unsigned int length;
+        Heap<K,V> maxHeap;
+
+    public:
+        PriorityQueue():length{0},maxHeap{ArrayHeap<K,V>{}}{};
+        unsigned int size();
+        bool enqueue(K& key, V& value);
+        V dequeue();
+
     };
 }
 #endif //CPPIMPL_PRIORITY_STRUCTURE_H
--- DataStructureAndBasicAlgorithms/CppImpl/src/priority_structure.cpp
+++ DataStructureAndBasicAlgorithms/CppImpl/src/priority_structure.cpp
@@ -1,5 +1,166 @@
 //
 // Created by pengcheng.zhang on 2019-08-07.
 //
-#include <priority_structure.h>
+#include "priority_structure.h"
+#include "exception.h"
 using namespace patrick;
+
+/*
+ * Heap
+ * */
+template <class K, class V>
+unsigned int Heap<K,V>::size() {
+    return length;
+}
+
+/*
+ * ArrayHeap
+ * */
+template <class K, class V>
+void ArrayHeap<K,V>::push(K &key, V &value) {
+    Item<K,V> item{key,value};
+    array.append(item);
+    this->length++;
+    unsigned int indexOfItem=this->length-1;
+    //down to top
+    while (parentOf(indexOfItem)!= nullptr && key>parentOf(indexOfItem)->key){
+        auto temp=*parentOf(indexOfItem);
+        *parentOf(indexOfItem)=array[indexOfItem];
+        array[indexOfItem]=temp;
+    }
+}
+
+template <class K, class V>
+V ArrayHeap<K,V>::pop() {
+    if (this->length==0){
+        throw OutofIndexException{};
+    }
+    auto result=array[0];
+    array[0]=array[this->length-1];
+    this->length--;
+    //top to down
+    maxHeapify(0);
+    return result;
+}
+
+template <class K, class V>
+V& ArrayHeap<K,V>::top() {
+    if (this->length==0){
+        throw OutofIndexException{};
+    }
+    return array[0].value;
+}
+
+template <class K, class V>
+void ArrayHeap<K,V>::maxHeapify(unsigned int index) {
+    unsigned int largest=index;
+    unsigned int left=index*2+1;
+    unsigned int right=2*(index+1);
+    if (left<this->length && array[left].key>array[index].key){
+        largest=left;
+    }
+    if (right<this->length && array[right].key>array[largest].key){
+        largest=right;
+    }
+    if (largest!=index){
+        auto temp=array[index];
+        array[index]=array[largest];
+        array[largest]=temp;
+        maxHeapify(largest);
+    }
+}
+
+template <class K, class V>
+Item<K,V>* ArrayHeap<K,V>::parentOf(unsigned int index) {
+    if (index >= this->length){
+        throw OutofIndexException{};
+    }
+    if (index == 0){
+        return nullptr;
+    }
+    return &array[(index-1)/2];
+}
+
+template <class K, class V>
+Item<K,V>* ArrayHeap<K,V>::leftOf(unsigned int index) {
+    if (index >= this->length){
+        throw OutofIndexException{};
+    }
+    unsigned int leftIndex=index*2+1;
+    if (leftIndex>=this->length){
+        return nullptr;
+    }
+    return &array[leftIndex];
+}
+
+template <class K, class V>
+Item<K,V>* ArrayHeap<K,V>::rightOf(unsigned int index) {
+    if (index >= this->length){
+        throw OutofIndexException{};
+    }
+    unsigned int rightIndex=(index+1)*2;
+    if (rightIndex>=this->length){
+        return nullptr;
+    }
+    return &array[rightIndex];
+}
+/*
+template <class K, class V>
+TreeHeap<K,V>::~TreeHeap() {
+    releaseTree(root);
+}
+
+template <class K, class V>
+TreeHeap<K,V>::TreeHeap(const TreeHeap<K, V> &heap) {
+    this->root=new HeapNode<K,V>{heap.root->key,heap.root->value, nullptr, nullptr, nullptr};
+    copyTree(this->root,heap.root);
+}
+
+template <class K, class V>
+TreeHeap<K,V>::TreeHeap(TreeHeap<K, V> &&heap) {
+    this->root=heap.root;
+    this->length=heap.length;
+    heap.root= nullptr;
+    heap.length=0;
+}
+
+template <class K, class V>
+TreeHeap<K,V>& TreeHeap<K,V>::operator=(const TreeHeap<K, V> &heap) {
+    releaseTree(this->root);
+    copyTree(this->root,heap.root);
+    this->length=heap.length;
+}
+
+template <class K, class V>
+TreeHeap<K,V>& TreeHeap<K,V>::operator=(TreeHeap<K, V> &&heap) {
+    releaseTree(this->root);
+    this->root=heap.root;
+    this->length=heap.length;
+    heap.root= nullptr;
+    heap.length=0;
+}
+
+template <class K, class V>
+void TreeHeap<K,V>::push(K &key, V &value) {
+
+}
+ */
+
+/*
+ * PriorityQueue based on ArrayHeap
+ * */
+template <class K, class V>
+unsigned int PriorityQueue<K,V>::size() {
+    return this->length;
+}
+
+template <class K, class V>
+bool PriorityQueue<K,V>::enqueue(K &key, V &value) {
+    maxHeap.push(key,value);
+}
+
+template <class K, class V>
+V PriorityQueue<K,V>::dequeue() {
+    return maxHeap.pop();
+}
+
